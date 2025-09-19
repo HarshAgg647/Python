beb6e23fbb35a3c62df6fbcbbbf7933b9f5c9d07
HarshAgg647 <harshaggarwal647@gmail.com>
2025-09-19 13:49:41 +0530

    Commited Successfully

---
+++ TuteDude/Assignment 3/Task2.py
@@ -0,0 +1,9 @@
+import math
+
+num = int(input("Enter a number: "))
+
+print("Square root of {0} is {1}".format(num, math.sqrt(num)))
+
+print("Logarithm of {0} is {1}".format(num, math.log(num)))
+
+print("Sine of {0} is {1}".format(num, math.sin(num)))
